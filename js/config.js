f0139ff8a050bdc0b0f63f3aef9c804263e9c5e6
Aurora Comercial <aurorartecomercial@gmail.com>
2026-08-25 23:14:08 +0100

    Atualizaçao do config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
 export const CONFIG = {
-    // URL base da sua API (mude para o domínio real ou localhost para testes)
-    API_BASE: 'http://localhost/api',  // Ex: 'https://aurora-api.infinityfreeapp.com/api'
-    
-    // Outras configurações
+    API_BASE: 'https://SEU-SUBDOMINIO.infinityfreeapp.com/api', 
+    // Substitua SEU-SUBDOMINIO pelo nome que criou (ex: aurora-api)
     CACHE_KEY: 'aurora_catalogo_cache',
     CACHE_TTL: 60 * 60 * 1000,
     NUMERO_WHATSAPP: '244933677628'
