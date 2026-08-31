5b3400f626381006710b7c2c8cf3d43c9dec077f
Aurora Comercial <aurorartecomercial@gmail.com>
2026-09-01 00:44:53 +0100

    m

--- functions/api/admin/_middleware.js
+++ functions/api/admin/_middleware.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
   const { request, env, next } = context;
   
-  // Se a rota NÃO começar por /api/admin, deixa passar
+  // Se a rota NÃO começar por /api/admin, deixa passar (permite HTML normal)
   const url = new URL(request.url);
   if (!url.pathname.startsWith('/api/admin')) {
     return next();
