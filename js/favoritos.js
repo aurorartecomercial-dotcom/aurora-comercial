9a90fed7655fb2e0f8d41c6d11d17b9881f3d5c9
Aurora Comercial <aurorartecomercial@gmail.com>
2026-09-09 21:39:26 +0100

    MartplaceV5

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,5 +1,5 @@
 // ============================================================
-// LISTA DE DESEJOS - AURORA COMERCIAL (VERSÃO MELHORADA)
+// LISTA DE DESEJOS - VORA 313 (VERSÃO MELHORADA)
 // ============================================================
 
 let favoritos = [];
